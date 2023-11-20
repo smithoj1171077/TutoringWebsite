630bb3a94d8a39f22570e97a5acb861f3c6cdda9
smithoj1171077 <140402070+smithoj1171077@users.noreply.github.com>
2023-11-20 11:15:47 +1100

    clean() restructured
    I separated the distinct blocks of custom clean logic into methods to enhance readability of main clean method.

--- ACETutoring/studentrequest/forms.py
+++ ACETutoring/studentrequest/forms.py
@@ -66,51 +66,37 @@ class StudentRequestForm(forms.ModelForm):
             return True
         return False
 
-            
-
-    def clean(self):
-        cleaned_data = super().clean()
-        #----------------at least one subject-------------------------
-        subject1 = cleaned_data.get('subject1') 
-        subject2 = cleaned_data.get('subject2_optional')
-        subject3 = cleaned_data.get('subject3_optional')
-        subject4 = cleaned_data.get('subject4_optional')
-        subjects = (subject1,subject2,subject3,subject4)
-            
-        if self.at_least_n(n=1, fields = subjects) == False:
+    # raises error if no subject is entered in any field     
+    def clean_no_filled_subjects(self,subjects):
+         if self.at_least_n(n=1, fields = subjects) == False:
             raise ValidationError(
                 'need to enter at least one subject'
                 )
-              
-        #---------------post code validation-----------------------------------
-        postcode = cleaned_data.get('postcode')
-        onlineOnly = cleaned_data.get('online_only')
-
-        pc_validity = PostCodeValidatorInterface.validate(postcode)
+         
+    # raises error if the post code is too far or not found
+    def clean_post_code(self,postcode,onlineOnly):
+         pc_validity = PostCodeValidatorInterface.validate(postcode)
     
-        if (pc_validity == False) and not onlineOnly:
-            raise ValidationError(
+         if (pc_validity == False) and not onlineOnly:
+              raise ValidationError(
                 'postcode too far'
             )
                
-        elif (pc_validity == "Postcode not found"):
-            raise ValidationError(
+         elif (pc_validity == "Postcode not found"):
+              raise ValidationError(
                 'no matching Australian post code found'
             )
-               
-        #-----------------------contact info validation-------------------------------
-        student_email = cleaned_data.get('students_email')
-        parent_email = cleaned_data.get('parent_or_guardian_email')
-        phone = cleaned_data.get('phone_number')
-
-        if not self.at_least_n(n=1,fields=(student_email,parent_email,phone)):
+         
+    # makes sure at least one piece of contact info is provided
+    def clean_contact_info(self,student_email,parent_email,phone):
+         if not self.at_least_n(n=1,fields=(student_email,parent_email,phone)):
             for field in (student_email,parent_email,phone):
                 raise ValidationError(
                     'at least one of student email, parent/guradian email, or phone number required'
                 )
-                    
-        #------------------------year level -> subject mapping validation-------------------------------------
-        year_level = cleaned_data.get('year_level')
+            
+    # makes sure that there is not a year level subject mismatch
+    def clean_year_level_subject_mismatch(self,year_level,subjects):
         for i in range(0, len(subjects)):
             if subjects[i] != None:
                 subject_year_level_match = False
@@ -121,6 +107,37 @@ class StudentRequestForm(forms.ModelForm):
                             raise ValidationError(
                                 'We do not provide tutoring services to this level of this subject unfortunately'
                                 )
+         
+         
+         
+    def clean(self):
+        cleaned_data = super().clean()
+        #----------------at least one subject-------------------------
+        subject1 = cleaned_data.get('subject1') 
+        subject2 = cleaned_data.get('subject2_optional')
+        subject3 = cleaned_data.get('subject3_optional')
+        subject4 = cleaned_data.get('subject4_optional')
+        subjects = (subject1, subject2, subject3, subject4)
+            
+        self.clean_no_filled_subjects(subjects)
+              
+        #---------------post code validation-----------------------------------
+        postcode = cleaned_data.get('postcode')
+        onlineOnly = cleaned_data.get('online_only')
+        self.clean_post_code(postcode, onlineOnly)
+        
+               
+        #-----------------------contact info validation-------------------------------
+        student_email = cleaned_data.get('students_email')
+        parent_email = cleaned_data.get('parent_or_guardian_email')
+        phone = cleaned_data.get('phone_number')
+        self.clean_contact_info(student_email, parent_email,phone)
+        
+                    
+        #------------------------year level -> subject mapping validation-------------------------------------
+        year_level = cleaned_data.get('year_level')
+        self.clean_year_level_subject_mismatch(year_level, subjects)
+        
         return cleaned_data
             
 
